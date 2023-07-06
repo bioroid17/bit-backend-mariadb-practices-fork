0188ffa2b7bb7ba2859fd1953b8deec7f4c46e65
BIT <BIT@DESKTOP-EJDVL85>
2023-07-06 11:52:43 +0900

    집계 - I

--- sql연습/날짜함수.sql
+++ sql연습/날짜함수.sql
@@ -14,4 +14,41 @@ select sysdate(), sleep(2), sysdate() from dual;
 -- date_format
 select date_format(now(), '%Y년 %m월 %d일 %h시 %i분 %s초') from dual;
 
-select date_format(now(), '%d') from dual;
+select date_format(now(), '%d %b \'%y') from dual;
+
+-- period_diff
+-- 포맷팅(YYMM, YYYYMM)
+-- 예) 근무 개월
+
+select emp_no,
+       hire_date,
+       period_diff(date_format(curdate(), '%y%m'), date_format(hire_date, '%y%m')) as month
+from employees;
+
+-- date_add(=adddate), date_sub(subdate)
+-- 시간을 date 타입의 컬럼이나 값에 type(year, month, day)의
+-- 표현식으로 더하거나 뺄 수 있다.
+-- 예) 각 사원의 근속 년 수가 5년이 되는 날에 휴가를 보내 준다면
+--    각 사원의 근속 휴가 날짜는?
+select first_name,
+       hire_date,
+       date_add(hire_date, interval 5 year)
+  from employees;     
+
+-- cast
+select '12345' + 10, cast('12345' as int) + 10
+  from dual;
+
+select date_format(cast('2023-07-06' as date), '%Y년 %m월 %d일')
+  from dual;
+
+select cast(cast(1-2 as unsigned) as signed) from dual;
+select cast(cast(1-2 as unsigned) as int) from dual;
+select cast(cast(1-2 as unsigned) as integer) from dual;
+
+-- type
+-- 문자: varchar, char, text, CLOB(Character Large OBject)
+-- 정수: tyny int, medium int, int(signed, integer), unsigned, big int
+-- 실수: float, double
+-- 시간: date, datetime
+-- LOB: CLOB, BLOB(Binary Large Object)     
