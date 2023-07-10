622a84dc75997b1b8a32a6609df4e3046d35ac3c
BIT <BIT@DESKTOP-EJDVL85>
2023-07-10 09:54:38 +0900

    PreparedStatement I

--- jdbc-practice/src/main/java/test/InsertTest02.java
+++ jdbc-practice/src/main/java/test/InsertTest02.java
@@ -2,11 +2,21 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class ConnectionTest {
+public class InsertTest02 {
+
 	public static void main(String[] args) {
+		insert("영업2");
+		insert("영업3");
+	}
+	
+	private static boolean insert(String deptName) {
+		boolean result = false;
+		
 		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
 		try {
 			//1. JDBC Driver Class 로딩
@@ -16,13 +26,29 @@ public class ConnectionTest {
 			String url = "jdbc:mariadb://192.168.0.155:3306/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
-			System.out.println("연결 성공");
+			//3. Statement 준비
+			String sql = "insert into dept values (null, ?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. 바인딩(binding)
+			pstmt.setString(1, deptName);
+			
+			//5. SQL 실행
+			int count = pstmt.executeUpdate();
+			
+			//6. 결과 처리
+			result = count == 1;
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
 		} catch (SQLException e) {
 			System.out.println("Error:" + e);
 		} finally {
+			//7. 자원 정리
 			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
 				if(conn != null) {
 					conn.close();
 				}
@@ -30,5 +56,8 @@ public class ConnectionTest {
 				e.printStackTrace();
 			}
 		}
+		
+		return result;
 	}
+
 }
