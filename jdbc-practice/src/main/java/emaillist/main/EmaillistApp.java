85b5ee24000d90448dd2cf420c43eaaec89b4537
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-07-11 07:34:58 +0900

    Update EmaillistApp.java

--- jdbc-practice/src/main/java/emaillist/main/EmaillistApp.java
+++ jdbc-practice/src/main/java/emaillist/main/EmaillistApp.java
@@ -10,43 +10,56 @@ public class EmaillistApp {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
 		while(true) {
 			System.out.print("(l)ist (d)elete (i)nsert (q)uit > ");
 			String command = scanner.nextLine();
 			
-			if("q".equals(command)) {
-				break;
-			} else if("l".equals(command)) {
+			if("l".equals(command)) {
 				doList();
-			} else if("d".equals(command)) {
-				doDelete();
 			} else if("i".equals(command)) {
 				doInsert();
+			} else if("d".equals(command)) {
+				doDelete();
+			} else if("q".equals(command)) {
+				break;
 			}
 		}
+		scanner.close();
 	}
-
+	
+	public static void doDelete() {
+		System.out.print("이메일:");
+		String email = scanner.nextLine();
+		
+		new EmaillistDao().deleteByEmail(email);
+		
+		doList();
+	}
+	
 	private static void doInsert() {
 		System.out.print("성:");
 		String firstName = scanner.nextLine();
 
 		System.out.print("이름:");
 		String lastName = scanner.nextLine();
-		
+
 		System.out.print("이메일:");
 		String email = scanner.nextLine();
-		
-		
-	}
 
-	private static void doDelete() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName(firstName);
+		vo.setLastName(lastName);
+		vo.setEmail(email);
+		
+		new EmaillistDao().insert(vo);
+		
+		doList();
 	}
-
+	
 	private static void doList() {
-		List<EmaillistVo> list= new EmaillistDao().findAll();
+		List<EmaillistVo> list = new EmaillistDao().findAll();
 		for(EmaillistVo vo : list) {
-			System.out.println(vo.getFirstName() + " " + vo.getLastName() + " - " + vo.getEmail());
+			System.out.println("이름:" + vo.getFirstName() + " " + vo.getLastName() + ", 이메일:" + vo.getEmail());
 		}
 	}
 }
