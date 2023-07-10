d515be22b6f7aaebab13fab4ac32b8451404e3c3
BIT <BIT@DESKTOP-EJDVL85>
2023-07-10 14:47:45 +0900

    Emaillist II

--- jdbc-practice/src/main/java/emaillist/dao/test/EmaillistDaoTest.java
+++ jdbc-practice/src/main/java/emaillist/dao/test/EmaillistDaoTest.java
@@ -8,14 +8,14 @@ import emaillist.vo.EmaillistVo;
 public class EmaillistDaoTest {
 
 	public static void main(String[] args) {
+		// Mock 객체 
 		EmaillistVo vo = new EmaillistVo();
 		vo.setFirstName("안");
 		vo.setLastName("대혁");
 		vo.setEmail("kickscar@gmail.com");
-		testInsert(vo);
-
-		testDelete("kickscar@gmail.com");
 		
+		testInsert(vo);
+		testDelete("kickscar@gmail.com");
 		testFindAll();
 	}
 
