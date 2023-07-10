d5cae48aa47b18b951679a66f2ca1ad8a217810a
비트부트캠프 <137239441+bitacademy-bootcamp23-backend@users.noreply.github.com>
2023-07-11 07:36:49 +0900

    Update BookDao.java

--- jdbc-practice/src/main/java/bookshop/dao/BookDao.java
+++ jdbc-practice/src/main/java/bookshop/dao/BookDao.java
@@ -3,45 +3,34 @@ package bookshop.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import bookshop.vo.BookVo;
 
+
 public class BookDao {
-	public boolean insert(BookVo vo) {
-		boolean result = false;
-		
+
+	public void insert(BookVo vo) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		
 		try {
-			//1. JDBC Driver Class 로딩
-			Class.forName("org.mariadb.jdbc.Driver");
+			conn = getConnection();
 			
-			//2. 연결하기
-			String url = "jdbc:mariadb://192.168.0.155:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
-			//3. Statement 준비
-			String sql = "insert into book values (null, ?, 'n', ?)";
-			
+			String sql = "insert into book(no, title, author_no) values(null, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			
-			//4. 바인딩(binding)
 			pstmt.setString(1, vo.getTitle());
 			pstmt.setLong(2, vo.getAuthorNo());
 			
-			//5. SQL 실행
-			int count = pstmt.executeUpdate();
-			
-			//6. 결과 처리
-			result = count == 1;
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
+			pstmt.executeUpdate();
 		} catch (SQLException e) {
-			System.out.println("Error:" + e);
+			System.out.println("error:" + e);
 		} finally {
-			//7. 자원 정리
+			// clean up
 			try {
 				if(pstmt != null) {
 					pstmt.close();
@@ -53,7 +42,107 @@ public class BookDao {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public List<BookVo> findAll() {
+		List<BookVo> result = new ArrayList<>();
 		
-		return result;		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = 
+				"   select a.no, a.title, a.rent, b.name as authorName" +
+				"     from book a, author b" +
+				"    where a.author_no = b.no" +
+				" order by no desc";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String title = rs.getString(2);
+				String rent = rs.getString(3);
+				String authorName = rs.getString(4);
+				
+				BookVo vo = new BookVo();
+				vo.setNo(no);
+				vo.setTitle(title);
+				vo.setRent(rent);
+				vo.setAuthorName(authorName);
+				
+				result.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// clean up
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public void update(BookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = 
+				"update book" +
+				"   set rent=?" +
+				" where no=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getRent());
+			pstmt.setLong(2, vo.getNo());
+			
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			String url = "jdbc:mariadb://192.168.10.125:3307/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+
+		return conn;
 	}
 }
